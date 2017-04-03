e3bef18760fa38f3606927e617de5ce58f176ef0
Rafael Tavares <rafaeltavaresandrade@gmail.com>
2017-04-03 14:14:13 -0300

    Adicionando arquivos css e js

--- ComponentesAngular/src/app/app.module.ts
+++ ComponentesAngular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DatepickerComponent } from './components/datepicker/datepicker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatepickerComponent
   ],
   imports: [
     BrowserModule,
